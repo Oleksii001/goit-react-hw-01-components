f5e7ae2d3dc2866dd4a96c21050fe8eb1da3d645
Oleksii001 <oleksii.burlaka@gmail.com>
2023-07-15 11:37:33 +0300

    finish-task-1-2-3-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import React from 'react';
+import data from '../components/Statistics/data';
+import user from '../components/Profile/user';
+import friends from '../components/FriendList/friends';
+import transactions from '../components/TransactionHistory/transactions';
+
+import Profile from './Profile/Profile.jsx';
+import Statistics from './Statistics/Statistics.jsx';
+import FriendList from './FriendList/FriendList.jsx';
+import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList list={friends}></FriendList>
+      <TransactionHistory items={transactions}></TransactionHistory>
     </div>
   );
 };
